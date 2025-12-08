bd3e7f02b9c99374b0785b11ce94ce6c30f99a8e
Anmol Arora <63544804+anmolarora013@users.noreply.github.com>
2025-12-08 11:04:26 +0000

    course done

---
+++ airflow/dags/basic_etl_dag.py
@@ -0,0 +1,46 @@
+'''Basic ETL DAG'''
+from datetime import datetime, date
+from airflow import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.providers.standard.operators.bash import BashOperator
+import pandas as pd
+
+with DAG(
+        'basic_etl_dag',
+        schedule=None,
+        start_date=datetime(2025, 1, 1),
+        default_args={
+            'owner': 'Anmol',
+            'description': 'A basic ETL DAG',
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 0
+        }
+) as dag:
+    extract_task = BashOperator(
+        task_id='extract_task',
+        bash_command='wget -c https://datahub.io/core/top-level-domain-names/r/top-level-domain-names.csv.csv -O /workspaces/hands-on-introduction-data-engineering-4395021/lab/end-to-end/airflow-extract-data.csv'
+    )
+
+    def transform_data():
+        """Read in the file and transformed file out"""
+
+        df = pd.read_csv('/workspaces/hands-on-introduction-data-engineering-4395021/lab/end-to-end/airflow-extract-data.csv')
+
+        generic_type_df = df[df['Type'] == 'generic']
+        generic_type_df['Date'] = date.today().strftime('%Y-%m-%d')
+        generic_type_df.to_csv('/workspaces/hands-on-introduction-data-engineering-4395021/lab/end-to-end/airflow-transform-data.csv', index=False)
+    
+    transform_task = PythonOperator(
+        task_id='transform_task',
+        python_callable=transform_data,
+        dag=dag
+    )
+
+    load_task = BashOperator(
+        task_id='load_task',
+        bash_command='echo -e ".separator ","\n.import --skip 1 /workspaces/hands-on-introduction-data-engineering-4395021/lab/end-to-end/airflow-transform-data.csv top_level_domains" | sqlite3 /workspaces/hands-on-introduction-data-engineering-4395021/lab/end-to-end/basic-etl-load-db.db',
+        dag=dag
+    )
+
+    extract_task >> transform_task >> load_task
